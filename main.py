b332ffa2f47d0909eae9018c60616720449f18de
Fraser Dwyer <fraserd1413@gmail.com>
2023-01-07 10:42:47 +0000

    human player lunar lander

---
+++ main.py
@@ -0,0 +1,46 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium.utils.play import play, PlayPlot
+
+from datetime import datetime, date
+
+num_episodes = 3
+env = gym.make("LunarLander-v2", render_mode="rgb_array")
+
+ep_rew = []
+rewards = []
+
+
+def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
+    global rewards, ep_rew
+    ep_rew.append(rew)
+    if terminated:
+        rewards.append(ep_rew)
+        ep_rew = []
+    return [rew,]
+
+
+def play_game(num_episodes):
+    play(num_episodes, env, keys_to_action={
+        "w": 2,
+        "a": 1,
+        "d": 3
+    }, noop=0, callback=callback)
+
+
+def calc_rewards(rewards, num_episodes):
+    time_now = datetime.now().strftime("%H:%M:%S")
+    date_now = date.today()
+    filename = str(date_now) + \
+        "-Keyboard-Agent-" + str(num_episodes) + \
+        "-Episodes-" + str(time_now) + ".csv"
+    print(filename)
+    file = open(filename, "w")
+    for each in rewards:
+        file.write(str(sum(each)))
+        file.write("\n")
+    file.close()
+
+
+play_game(num_episodes)
+calc_rewards(rewards, num_episodes)
